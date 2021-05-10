a3a8b77e4783561e5d2cdd13d46ad84741d2203c
Rauf Hajiyev <hrauf1000@gmail.com>
2021-05-10 13:06:52 +0200

    gitignore updated

--- src/main/java/pages/PageBase.java
+++ src/main/java/pages/PageBase.java
@@ -3,11 +3,6 @@ package pages;
 import sites.Hackerrank;
 import utilities.AutomationTool;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
 public class PageBase {
     protected AutomationTool webTool;
 
@@ -21,31 +16,5 @@ public class PageBase {
 
     public String getPageUrl() {
         return Hackerrank.siteUrl;
-    }
-
-    private static Properties getProperties() {
-        String propFileName = "config.properties";
-        InputStream inputStream = null;
-        Properties prop = new Properties();
-        try {
-            inputStream = AutomationTool.class.getClassLoader().getResourceAsStream(propFileName);
-
-            if (inputStream != null)
-                prop.load(inputStream);
-            else
-                throw new FileNotFoundException("Property file '" + propFileName + "' not found in the classpath");
-
-        } catch (Exception e) {
-            System.out.println("Exception: " + e);
-        } finally {
-            try {
-                inputStream.close();
-            } catch (IOException e) {
-                System.out.println("Exception: " + e);
-                e.printStackTrace();
-            }
-        }
-
-        return prop;
     }
 }
